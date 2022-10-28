cafe1e17bba1516313fa68d1895251f79446e875
ilya <rudakov.alhan@gmail.com>
2022-10-28 18:20:47 +0300

    feat: add soluiton task 1

--- unit_3/oop-3_blank/unit_03/class/Button.js
+++ unit_3/oop-3_blank/unit_03/class/Button.js
@@ -1,7 +1,15 @@
 class Button {
 
+    constructor(width, heigth, background, value) {
+        this.width = width;
+        this.heigth = heigth;
+        this.background = background;
+        this.value = value;
+    }
 
     render() {
+
+        const button = document.createElement('button');
 
     }
 }
