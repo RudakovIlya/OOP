c0d49f1abeb02bf124ae0cdfa9aeddd596a518c2
ilya <rudakov.alhan@gmail.com>
2022-10-28 18:30:16 +0300

    feat: add solution task 2

--- unit_3/oop-3_blank/unit_03/class/Button.js
+++ unit_3/oop-3_blank/unit_03/class/Button.js
@@ -1,8 +1,8 @@
 class Button {
 
-    constructor(width, heigth, background, value) {
+    constructor(width, height, background, value) {
         this.width = width;
-        this.heigth = heigth;
+        this.height = height;
         this.background = background;
         this.value = value;
     }
@@ -11,5 +11,18 @@ class Button {
 
         const button = document.createElement('button');
 
+        button.setAttribute('type', 'button');
+
+        button.style.background = this.background;
+
+        button.textContent = this.value;
+
+        button.style.width = this.width + 'px';
+
+        button.style.height = this.height + 'px';
+
+        document.querySelector('.button').append(button);
     }
 }
+
+const button = new Button(200, 50, 'green', 'new Button').render();
